35805b76b08abb46ee241e01845e1fd8ba926a7e
jon borgman <jon.borgman@lnssi.com>
2021-03-30 17:28:12 -0400

    P1 M1

---
+++ Project1/Milestone 2/js/main.js
@@ -0,0 +1,9 @@
+const topRockAlbums = [
+    { artist: "Queen", title: "Greatest Hits", eq_albums: 929000 },
+    { artist: "Elton John", title: "Diamonds", eq_albums: 743000 },
+    { artist: "Fleetwood Mac", title: "Rumours", eq_albums: 721000 },
+    { artist: "CCR", title: "The 20 Greatest Hits", eq_albums: 630000 },
+    { artist: "Journey", title: "Journey's Greatest Hits", eq_albums: 561000 }
+ ];
+
+ const topAlbumsSection = d3.select('#top-albums');
