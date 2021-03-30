9589def4c7716b227def4bd5f83a05bdb1766d00
jon borgman <jon.borgman@lnssi.com>
2021-03-30 18:51:30 -0400

    P1 M1

--- Project1/Milestone 2/js/main.js
+++ Project1/Milestone 2/js/main.js
@@ -9,5 +9,29 @@ const topRockAlbums = [
  const topAlbumsSection = d3.select('#top-albums');
 
  topAlbumsSection
-      .append('h3')
-         .text('Top Rock Albums');
+    .append('h3')
+        .text('Top Rock Albums');
+//
+const barChartWidth = 500;
+const barChartHeight = 130;
+const barChart = topAlbumsSection
+    .append('svg')
+        .attr('viewbox', [0, 0, barChartWidth, barChartHeight])
+        .attr('width', barChartWidth)
+        .attr('height', barChartHeight);
+
+const paddingLeft = 200;
+barChart
+    .append('line')
+        .attr('x1', paddingLeft)
+        .attr('y1', 0)
+        .attr('x2', paddingLeft)
+        .attr('y2', barChartHeight)
+        .attr('stroke', '#333')
+        .attr('stroke-width', 2);
+
+const barLenghtScale = d3.scaleLinear()
+    .domain([0, 1000000]) // In our data, the number of album-equivalent goes up to about 1,000,000
+    .range([0, barChartWidth - paddingLeft - 100]); // Based on the space that we have on screen and the space we need for the labels
+
+//console.log(barLenghtScale(700000));
