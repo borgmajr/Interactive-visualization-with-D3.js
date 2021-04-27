919d7f314c6098018f661c845d759e87cb9df638
jon borgman <jon.borgman@lnssi.com>
2021-04-27 17:50:58 -0400

    P2 M2

--- Project2/Milestone 2/js/gender-gap.js
+++ Project2/Milestone 2/js/gender-gap.js
@@ -57,8 +57,50 @@ createViz = (data) => {
   console.log("maxNumber", maxNumber);
   console.log("maxBucketSize", maxBucketSize);
 
-  const maxXScale = binContainer.filter(o => o.maxNumber === maxNumber);
-  console.log("maxXScale", maxXScale);
+  const maxWidth = 60;
+
+  // const maxYScale = binContainer.filter(o => o.maxNumber === maxNumber)[0].yScale;
+  // console.log("maxYScale", maxYScale);
+
+  // const xScale = d3.scaleLinear()
+  //   .domain([0, maxBucketSize])
+  //   .range([0, (maxWidth*6)]);
+
+  var xScale = d3.scaleBand()
+      .domain(binContainer.map(o => o.sport))
+      .range([ 0, width - margin.left - margin.right ])
+      .padding(0.05)   
+
+  const yScale = d3.scaleLinear()
+    .domain([0, maxNumber + 1000000])
+    .range([height - margin.bottom - margin.top, margin.top]);
+
+
+  const vizDiv = d3.select('#viz');
+
+  const vizChart = vizDiv
+      .append('svg')
+          .attr('viewbox', [0, 0, width, height])
+          .attr('width', width)
+          .attr('height', height);    
+
+  vizChart
+      .append('g')
+          .attr('transform', `translate(${margin.left}, ${height - margin.bottom - margin.top})`)
+          .call(d3.axisBottom(xScale));   
+  
+      
+  vizChart
+      .append('g')
+          .attr('transform', 'translate('+margin.left+', 0)')
+          .call(d3.axisLeft(yScale));
+
+  vizChart
+          .append('text')
+              .attr('text-anchor', 'start')
+              .attr('x', margin.left)
+              .attr('y', margin.top)
+              .text('Earnings in 2019 (USD)');
 
 };
 
@@ -69,24 +111,24 @@ function injectScales(binContainer){
 
       binContainer.maxNumber = maxNumber;
 
-      let xScale = d3.scaleLinear()
-              .domain([0, maxNumber])
-              .range([0, width - margin.left - margin.right - 30])
-              .nice();
+      // let xScale = d3.scaleLinear()
+      //         .domain([0, maxNumber])
+      //         .range([0, width - margin.left - margin.right - 30])
+      //         .nice();
 
-              binContainer.xScale = xScale;
+      // binContainer.xScale = xScale;
   
       const maxBucketSize = Math.max(...binContainer.bins.flatMap(o => o.length));
       //console.log("maxBucketSize",maxBucketSize);
 
       binContainer.maxBucketSize = maxBucketSize;
 
-      let yScale = d3.scaleLinear()
-          .domain([maxBucketSize, 0])
-          .range([10, height - margin.top - margin.bottom])
-          .nice();
-  
-      //console.log("xScale",xScale);
+      // let yScale = d3.scaleLinear()
+      //     .domain([maxBucketSize, 0])
+      //     .range([10, height - margin.top - margin.bottom])
+      //     .nice();
 
-      binContainer.xScale = xScale;
+      // //console.log("xScale",xScale);
+
+      // binContainer.yScale = yScale;
 }
